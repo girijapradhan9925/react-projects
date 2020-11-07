b7731134b145acb501bc431b061fa8d1fb8d11df
girijapradhan9925 <46650353+girijapradhan9925@users.noreply.github.com>
2020-11-07 23:13:43 +0530

    bcg changed

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,45 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebar, setIsSideBar] = useState(false);
+  const [isModal, setIsModal] = useState(false);
+
+  const openSidebar = () => {
+    setIsSideBar(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSideBar(false);
+  };
+
+  const openModal = () => {
+    setIsModal(true);
+  };
+
+  const closeModal = () => {
+    setIsModal(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        openSidebar,
+        openModal,
+        closeModal,
+        closeSidebar,
+        isModal,
+        isSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, eos alias
+        consectetur, rem saepe soluta maxime, deleniti nulla architecto
+        reiciendis ex in quos molestias velit neque mollitia nisi voluptatibus
+        inventore accusantium voluptas dignissimos? Tempore velit ipsum officiis
+        exercitationem, et reiciendis.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
